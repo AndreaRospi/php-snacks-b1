13ca40ce83b91a88da11dbeed1d366ea415dfad8
Andrea <andr3z45@gmail.com>
2022-01-18 18:09:39 +0100

    snack 2 completato

--- snack-2/index.php
+++ snack-2/index.php
@@ -1,10 +1,10 @@
 <?php 
 $nome = $_GET['nome'];
 $mail = $_GET['mail'];
-$età = $_GET['età'];
+$eta = $_GET['eta'];
 
-if(isset ($nome && $mail && $età) ){
-    if(strlen($nome) > 3 && strpos($mail , '@')!== false && strpos($mail , '.')!== false && is_numeric($età) ){
+if(isset ($nome) && isset($mail) && isset ($eta) ){
+    if(strlen($nome) > 3 && strpos($mail , '@')!== false && strpos($mail , '.')!== false && is_numeric($eta) ){
         echo "Accesso Riuscito";
     }else{
         echo "Accesso Negato";
